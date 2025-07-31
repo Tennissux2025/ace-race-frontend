0a0a106ae8b8d576bd62632e0c1e65d021c60076
Tennissux2025 <ighidirmic@gmail.com>
2025-07-31 22:04:19 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
+// vite.config.js (root)
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-    strictPort: true,
-    allowedHosts: "all",
-  },
 });
